39521306f24a01ee55729861a7cfac0681b6ac4f
NathanielRand <nathanieljrand@gmail.com>
2019-12-30 15:15:06 -0500

    Dispatch actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,38 @@
 import { createStore } from "redux";
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 2
+      };
+    case "DECREMENT":
+      return {
+        count: state.count - 1
+      };
+    case "RESET":
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
+});
+
+console.log(store.getState());
+
+// Actions - An object that gets sent to the store
+
+store.dispatch({
+  type: "INCREMENT"
+});
+
+store.dispatch({
+  type: "DECREMENT"
+});
+
+store.dispatch({
+  type: "RESET"
 });
 
 console.log(store.getState());
