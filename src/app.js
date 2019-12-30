0f0e6411bb3f9fa6216bc0cec3465c120aa68269
NathanielRand <nathanieljrand@gmail.com>
2019-12-29 20:41:36 -0500

    Implement react router web (dom)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 // import {insert main app component} from './components/AppName';
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>This is boilerplate</p>, document.getElementById("app"));
+const BudgetFYIDashboardPage = () => <div>Welcome to BudgetFYI</div>;
+
+const AddExpensePage = () => <div>Add Expense Page</div>;
+
+const EditExpensePage = () => <div>Edit Expense Page</div>;
+
+const AboutPage = () => <div>About Page</div>;
+
+const ContactPage = () => <div>Contact Page</div>;
+
+const HelpPage = () => <div>Help Page</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={BudgetFYIDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/about" component={AboutPage} />
+      <Route path="/contact" component={ContactPage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
