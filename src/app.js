e9c045b4923e55300c00c842b88fc8871755cfbc
NathanielRand <nathanieljrand@gmail.com>
2020-01-04 18:04:33 -0500

    Setup react-redux; Configure store on expense list and dashboard

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { addExpense } from "./actions/expenses";
@@ -16,10 +17,19 @@ store.dispatch(
 store.dispatch(
   addExpense({ description: "Power Bill", amount: 5000, createdAt: 1500 })
 );
+store.dispatch(
+  addExpense({ description: "Phone Bill", amount: 10000, createdAt: 1600 })
+);
 store.dispatch(setTextFilter("pow"));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById("app"));
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,10 @@
 import React from "react";
+import ExpenseList from "./ExpenseList";
 
-const DashboardPage = () => <div>Welcome to BudgetFYI</div>;
+const DashboardPage = () => (
+  <div>
+    <ExpenseList />
+  </div>
+);
 
 export default DashboardPage;
