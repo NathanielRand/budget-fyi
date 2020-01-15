ba3b6da829719060650b83d8be03aa6dc4f888ca
NathanielRand <nathanieljrand@gmail.com>
2020-01-15 13:25:22 -0500

    Test add expense page; Test edit expense page

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import ExpenseForm from "../../components/ExpenseForm";
 import expenses from "../fixtures/expenses";
+import moment from "moment";
 
 test("Should render ExpenseForm correctly", () => {
   const wrapper = shallow(<ExpenseForm />);
@@ -56,7 +57,7 @@ test("Should set amount if valid input", () => {
   expect(wrapper.state("amount")).toBe(value);
 });
 
-test("Should set description on input change", () => {
+test("Should not set amount if invalid input", () => {
   const value = "12.122";
   const wrapper = shallow(<ExpenseForm />);
   wrapper
@@ -66,4 +67,35 @@ test("Should set description on input change", () => {
       target: { value }
     });
   expect(wrapper.state("amount")).toBe("");
+});
+
+test("Should call onSubmit prop for valid form submission", () => {
+  const onSubmitSpy = jest.fn();
+  const wrapper = shallow(
+    <ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy} />
+  );
+  wrapper.find("form").simulate("submit", {
+    preventDefault: () => {}
+  });
+  expect(wrapper.state("error")).toBe("");
+  expect(onSubmitSpy).toHaveBeenLastCalledWith({
+    description: expenses[0].description,
+    amount: expenses[0].amount,
+    note: expenses[0].note,
+    createdAt: expenses[0].createdAt
+  });
+});
+
+test("Should set new date on date change", () => {
+  const now = moment();
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find("SingleDatePicker").prop("onDateChange")(now);
+  expect(wrapper.state("createdAt")).toEqual(now);
+});
+
+test("Should set calender focus on change", () => {
+  const focused = true;
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find("SingleDatePicker").prop("onFocusChange")({ focused });
+  expect(wrapper.state("calendarFocused")).toBe(focused);
 });
