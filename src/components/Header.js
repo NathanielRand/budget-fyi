a12b9fd33f595f30d702bf777ff5779017f82763
NathanielRand <nathanieljrand@gmail.com>
2020-02-22 01:52:07 -0500

    Style login page, header, page header, and expense filters

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,22 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { startLogout } from "../actions/auth";
 
 export const Header = ({ startLogout }) => (
-  <header>
-    <h1>
-      Budget<b className="header-title-fyi">FYI</b>
-    </h1>
-    <NavLink to="/dashboard" activeClassName="is-active">
-      Dashboard
-    </NavLink>
-    <NavLink to="/create" activeClassName="is-active">
-      Add Expense
-    </NavLink>
-    <NavLink to="/about" activeClassName="is-active">
-      About
-    </NavLink>
-    <NavLink to="/contact" activeClassName="is-active">
-      Contact
-    </NavLink>
-    <NavLink to="/help" activeClassName="is-active">
-      Help
-    </NavLink>
-    <button onClick={startLogout}>Logout</button>
+  <header className="header">
+    <div className="content-container">
+      <div className="header__content">
+        <Link to="/dashboard">
+          <h1 className="header__title">
+            Budget<b className="header__title-fyi">FYI</b>
+          </h1>
+        </Link>
+        <button className="header__button" onClick={startLogout}>
+          LOGOUT
+        </button>
+      </div>
+    </div>
   </header>
 );
 
