c4903c53f7824f3bfab1fc8b531f60150d5c65bd
NathanielRand <nathanieljrand@gmail.com>
2020-03-02 15:37:54 -0500

    Change font weight

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,16 +3,23 @@ import { connect } from "react-redux";
 import { startLogin } from "../actions/auth";
 
 export const LoginPage = ({ startLogin }) => (
-  <div className="box-layout">
-    <div className="box-layout__box">
-      <h1 className="box-layout__title">
-        Budget<fyi className="landing-title-fyi">FYI</fyi>
-      </h1>
-      <p className="box-layout__sub-title">Simplify Your Expenses</p>
-      <button className="box-layout__button" onClick={startLogin}>
-        LOGIN / SIGNUP
-      </button>
+  <div className="login">
+    <div className="box-layout">
+      <div className="box-layout__box">
+        <h1 className="box-layout__title">
+          Budget<fyi className="landing-title-fyi">FYI</fyi>
+        </h1>
+        <p className="box-layout__sub-title">Simplify Your Expenses</p>
+        <button className="box-layout__button" onClick={startLogin}>
+          LOGIN / SIGNUP
+        </button>
+      </div>
     </div>
+    {/* <div className="login-content">
+      <div className="content-container">
+        <div className="login-content__title">Simple & Secure Budgeting</div>
+      </div>
+    </div> */}
   </div>
 );
 
